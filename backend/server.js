8c183c7a094a86779ad12963dbe7a1b26bc86f77
= <kaweesha.mr@gmail.com>
2023-11-30 11:17:49 +0530

    Add basic route to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ const express = require('express')
 //setup the express app
 const app = express();
 
+
+
+//setting up the basic route to test
+app.get('/', (req,res) => {
+    res.json({mssg:'welcome to the app'})
+
+})
+
+
 //listen for requests
 app.listen(4000, () =>{
     console.log("listening in 4000")
