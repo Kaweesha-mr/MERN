9b0a840857c166bcbfa9953aacb20125fa054d23
= <kaweesha.mr@gmail.com>
2023-12-01 12:40:03 +0530

    frontend backend connected and fetched data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
 
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 //pages and components
 
 import Home from './pages/home'
-
+import Navbar from './components/navBar';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <div className="pages">
-        <Routes>
-          
-          <Route
-            path="/"
-            element= {<Home />}
-          />
-          
-        </Routes>
-      </div>
+
+        <Navbar />
+
+        <div className="pages">
+
+          <Routes>
+
+            <Route
+              path="/"
+              element={<Home />}
+            />
+
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
