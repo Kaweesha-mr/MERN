d1a53138717c21fa291406527349bb3176ea6631
= <kaweesha.mr@gmail.com>
2023-12-01 10:46:41 +0530

    tested request

--- backend/Controller/workoutController.js
+++ backend/Controller/workoutController.js
@@ -1,4 +1,5 @@
 const Workout = require('../models/Workout')
+const mongoose = require('mongoose')
 
 //get all workouts
 
@@ -14,12 +15,15 @@ const getWorkout = async (req,res) => {
 
     const {id} = req.params
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "invalid id"})
+    }
+
     const workout = await Workout.findById(id)
 
     if(!workout){
         return res.status(404).json({error: "no such workout"})
     }
-
     res.status(200).json(workout)
 
 }
