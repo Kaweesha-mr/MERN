2336453c25d9cfeb4d95d33c7df46044396469d7
= <kaweesha.mr@gmail.com>
2023-12-01 11:58:16 +0530

    fixed an error in react internel server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
+
+//pages and components
+
+import Home from './pages/home'
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <div className="pages">
+        <Routes>
+          
+          <Route
+            path="/"
+            element= {<Home />}
+          />
+          
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
